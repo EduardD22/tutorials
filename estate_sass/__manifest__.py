3f74d2ef24b982b6bc4be9caf4cb122ee271582f
Eduard Adrian Dinu <eduardadrian.dinu@gmail.com>
2025-07-24 15:59:54 +0200

    Write Importable Modules - Created estate property, estate property type and estate property tag models. Created their views, the security file and added them to the manifest

---
+++ estate_sass/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Estate Sass",
+    'depends': ['base'],
+    'data': [
+        "security/ir.model.access.csv",
+        "models/estate_property_tag.xml",
+        "models/estate_property_type.xml",
+        "models/estate_property.xml", 
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml", 
+    ],
+    'application': True
+}
